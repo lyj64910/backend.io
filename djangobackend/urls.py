4a08128dd4b043f1f90f54743975786b2d979729
lyj64910 <49028599+lyj64910@users.noreply.github.com>
2019-12-27 20:41:00 +0900

    Add files via upload

---
+++ djangobackend/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+import post.views 
+
+router = routers.DefaultRouter()
+router.register('posts', post.views.PostViewset)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
